881829c95e5f2c169d6dcfdbbea56cc084f0b4e3
Natalalia <n.olmo.megias@gmail.com>
2020-02-08 01:22:28 +0000

    get ready for test

--- db/data/readData.js
+++ db/data/readData.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 let data;
 
 try {
-  let dataContent = fs.readFileSync("./data/data.yml", "utf8");
+  let dataContent = fs.readFileSync("./db/data/data.yml", "utf8");
   data = yaml.safeLoad(dataContent);
 } catch (error) {
   console.log(error);
